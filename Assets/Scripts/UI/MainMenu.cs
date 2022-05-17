43dadd9ce6ff3437b3b3ba23edcf03b6021f9e57
Clymiaru <sekkadica@gmail.com>
2022-05-17 18:26:03 +0800

    Initial Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private SceneID MainGameScene;
+    
+        public void PlayGame()
+        {
+        }
+    }
+}
+
