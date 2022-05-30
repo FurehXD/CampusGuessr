0a9ad8fb3304eb6db1ce4906b469c123085d2fe0
Rylan Lim <rylan_lim@dlsu.edu.ph>
2022-05-30 13:55:25 +0800

    Revert "Merge pull request #2 from Clymiaru/Lanz"
    This reverts commit 5d03cccb673a7f7c570b11464ebc2b9d24a62609, reversing
    changes made to c6b35515a7c60f264f916f0b1ec328bfce6ae306.

--- Assets/Scripts/SceneID.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneID : ScriptableObject
-{
-    [SerializeField] private string sceneName;
-    public string SceneName => sceneName;
-}
